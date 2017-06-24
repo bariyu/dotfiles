bb3db7d095c42c75151e59bc141c1fd34ff48098
Baran Kucukguzel <kucukguzelbaran@gmail.com>
2017-06-24 21:56:47 +0200

    added brew.sh and .bash_profile

---
+++ brew.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+brew install bash
+brew install wget --with-iri
+brew install git
+brew install python
+brew install sqlite
+brew install tig
+brew install tree
+brew install tmux
+brew install htop-osx
+brew install openssl
+brew install jq
+brew install rsync
+brew install bash-completion2
+
+brew cleanup
